5c647a7bcb6500b7663ce7e9fc8b1421f0a5adcd
frannotti <frannotti10@gmail.com>
2026-03-21 18:05:39 -0300

    Agregado de puntajes

--- questions.py
+++ questions.py
@@ -13,6 +13,7 @@ words = [
 word = random.choice(words)
 guessed = []
 attempts = 6
+score = 0
 
 print("¡Bienvenido al Ahorcado!")
 print()
@@ -30,6 +31,7 @@ while attempts > 0:
     # Verificar si el jugador ya adivinó la palabra completa
     if "_" not in progress:
        print("¡Ganaste!")
+       score += 6
        break
     print(f"Intentos restantes: {attempts}")
     print(f"Letras usadas: {', '.join(guessed)}")
@@ -48,7 +50,10 @@ while attempts > 0:
     else:
         guessed.append(letter)
         attempts -= 1
+        score -= 1
         print("Esa letra no está en la palabra.")
     print()
 else:
+    score = 0
     print(f"¡Perdiste! La palabra era: {word}")
+print(f"puntaje final: {score}")
